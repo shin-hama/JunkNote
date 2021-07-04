96ca780708785f0c0880ad75696f0df0a43f1e04
shin-hama <htysivista@gmail.com>
2021-07-05 08:35:45 +0900

    First commit, make to run typescript and react

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+});
+
+module.exports = {
+  //  development or production
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        use: [
+          {
+            // babel
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    open: true,
+    port: 3000,
+  },
+};
