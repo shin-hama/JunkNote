1bfe2761914bc78212075ef0aca8f086eda674df
shin-hama <htysivista@gmail.com>
2021-07-05 11:20:29 +0000

    MAININT: Add eslint settings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import MemoCard from './MemoCard'
 
-export default function App() {
-    return (
+export default function App () {
+  return (
         <div className="reactApp">
            <MemoCard />
         </div>
-    )
+  )
 }
