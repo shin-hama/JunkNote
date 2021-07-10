2493e26d5781ee5dbeb8b7bb27719c24c0c92d0c
shin-hama <htysivista@gmail.com>
2021-07-10 05:48:28 +0000

    FEAT: Add theme palette

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,74 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 
 import './index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 
+const customTheme = createMuiTheme({
+  mixins: {
+    toolbar: {
+      minHeight: 48,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#1A535C',
+      contrastText: '#F7FFF7',
+    },
+    secondary: {
+      main: '#FFE047',
+      contrastText: '#1A535C',
+    },
+    error: {
+      main: '#FF6B6B',
+      contrastText: '#F7FFF7',
+    },
+    warning: {
+      main: '#FFE66D',
+      contrastText: '#1A535C',
+    },
+    info: {
+      main: '#24737F',
+      contrastText: '#F7FFF7',
+    },
+    success: {
+      main: '#5892DA',
+      contrastText: '#F7FFF7',
+    },
+  },
+  props: {
+    MuiTextField: {
+      variant: 'outlined',
+    },
+    MuiList: {
+      dense: true,
+    },
+    MuiCheckbox: {
+      color: 'primary',
+    },
+    MuiRadio: {
+      color: 'primary',
+    },
+    MuiSwitch: {
+      color: 'primary',
+    },
+  },
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <MuiThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
